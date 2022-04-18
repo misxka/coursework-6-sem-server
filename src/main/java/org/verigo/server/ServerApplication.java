03f0194a53530fe0d84bd5f2f6339cde1fe9e12e
misxka <mishutkavlesu@gmail.com>
2022-04-18 22:30:15 +0300

    feat: configure cloudinary storage

--- src/main/java/org/verigo/server/ServerApplication.java
+++ src/main/java/org/verigo/server/ServerApplication.java
@@ -1,11 +1,18 @@
 package org.verigo.server;
 
+import com.cloudinary.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(ServerApplication.class, args);
+
+        Cloudinary cloudinary = new Cloudinary(ctx.getEnvironment().getProperty("cloudinary.url"));
+        SingletonManager manager = new SingletonManager();
+        manager.setCloudinary(cloudinary);
+        manager.init();
     }
 }
