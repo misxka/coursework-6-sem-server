d31ca000e886445021d130ea9eea2a591e824508
misxka <mishutkavlesu@gmail.com>
2022-04-29 10:17:54 +0300

    feat: remove unused

--- src/main/java/org/verigo/server/data/repositories/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package org.verigo.server.data.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import org.verigo.server.data.entities.Group;
-
-public interface GroupRepository extends CrudRepository<Group, Integer> {
-}
